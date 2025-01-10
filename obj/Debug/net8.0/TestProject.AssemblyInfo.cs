bfb76739030550b6805277179ec8174b437f2271
Cristoffer Östberg <cristoffer_ostberg85@hotmail.com>
2025-01-10 22:04:39 +0100

    Methods W Parameters

--- obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74db86d957d9d701f58119f4349a87f088283662")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c03d34da9e34ecba922278a2bfde7368ffc77af9")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
