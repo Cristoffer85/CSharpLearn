ffc41754cf4dc32c1ca075258f99c629592c5d49
Cristoffer Östberg <cristoffer_ostberg85@hotmail.com>
2025-01-13 12:47:00 +0100

    Debug session

--- obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7c55219aca9e31d7e800865d35131c614abb4a8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0efa8eda72a52ecbc2498a03395ed7a743fb02ab")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
